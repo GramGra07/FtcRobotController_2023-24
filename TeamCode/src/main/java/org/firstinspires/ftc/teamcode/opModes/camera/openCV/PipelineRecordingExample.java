0cb080dc0ba163e4cd0cb2021f1ed621493e4a8a
Graden <101433010+GramGra07@users.noreply.github.com>
2023-06-13 16:40:55 -0600

    name changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/camera/openCV/PipelineRecordingExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/camera/openCV/PipelineRecordingExample.java
@@ -26,9 +26,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -58,7 +55,7 @@ public class PipelineRecordingExample extends LinearOpMode {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
 
-        phoneCam.setPipeline(new SamplePipeline());
+        phoneCam.setPipeline(new RecordingPipeline());
         phoneCam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
         phoneCam.setViewportRenderer(OpenCvCamera.ViewportRenderer.GPU_ACCELERATED);
         phoneCam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
@@ -105,25 +102,12 @@ public class PipelineRecordingExample extends LinearOpMode {
     }
 
 
-    class SamplePipeline extends OpenCvPipeline {
+    class RecordingPipeline extends OpenCvPipeline {
         boolean toggleRecording = false;
 
         @Override
         public Mat processFrame(Mat input) {
-            /*
-             * Draw a simple box around the middle 1/2 of the entire frame
-             */
-            Imgproc.rectangle(
-                    input,
-                    new Point(
-                            input.cols() / 4,
-                            input.rows() / 4),
-                    new Point(
-                            input.cols() * (3f / 4f),
-                            input.rows() * (3f / 4f)),
-                    new Scalar(0, 255, 0), 4);
-
-            return input;
+            return null;
         }
 
         @Override
