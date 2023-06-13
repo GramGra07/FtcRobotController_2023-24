745437d63ef3d9a52d1e801631a26aa87a82b0ec
Graden <101433010+GramGra07@users.noreply.github.com>
2023-06-13 16:41:43 -0600

    name changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/camera/openCV/pipelineTester.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.opModes.camera.openCV;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@TeleOp
+public class pipelineTester extends LinearOpMode {
+    OpenCvWebcam webcam;
+
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        OpenCVpipelines.detectionColor = "yellow";//must set a color
+        webcam.setPipeline(new OpenCVpipelines.ColorEdgeDetectionBounded());//!can switch pipelines here
+
+        webcam.setMillisecondsPermissionTimeout(5000); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+            }
+        });
+
+        telemetry.addLine("Waiting for start");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Frame Count", webcam.getFrameCount());
+            telemetry.addData("FPS", "%.2f", webcam.getFps());
+            //telemetry.addData("Total frame time ms", webcam.getTotalFrameTimeMs());
+            //telemetry.addData("Pipeline time ms", webcam.getPipelineTimeMs());
+            //telemetry.addData("Overhead time ms", webcam.getOverheadTimeMs());
+            //telemetry.addData("Theoretical max FPS", webcam.getCurrentPipelineMaxFps());
+            telemetry.update();
+            if (gamepad1.a) {
+                /*
+                 * IMPORTANT NOTE: calling stopStreaming() will indeed stop the stream of images
+                 * from the camera (and, by extension, stop calling your vision pipeline). HOWEVER,
+                 * if the reason you wish to stop the stream early is to switch use of the camera
+                 * over to, say, Vuforia or TFOD, you will also need to call closeCameraDevice()
+                 * (commented out below), because according to the Android Camera API documentation:
+                 *         "Your application should only have one Camera object active at a time for
+                 *          a particular hardware camera."
+                 *
+                 * NB: calling closeCameraDevice() will internally call stopStreaming() if applicable,
+                 * but it doesn't hurt to call it anyway, if for no other reason than clarity.
+                 *
+                 * NB2: if you are stopping the camera stream to simply save some processing power
+                 * (or battery power) for a short while when you do not need your vision pipeline,
+                 * it is recommended to NOT call closeCameraDevice() as you will then need to re-open
+                 * it the next time you wish to activate your vision pipeline, which can take a bit of
+                 * time. Of course, this comment is irrelevant in light of the use case described in
+                 * the above "important note".
+                 */
+                webcam.stopStreaming();
+                webcam.closeCameraDevice();
+            }
+            sleep(100);
+        }
+    }
+}
