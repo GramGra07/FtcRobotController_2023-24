e785470962a60b4e94235f28cfd39ac1e4298995
Graden <101433010+GramGra07@users.noreply.github.com>
2023-10-30 20:27:49 -0600

    add parking autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/auto/parkAuto/parkAutoRR.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opModes.auto.parkAuto;
+
+import static org.firstinspires.ftc.teamcode.opModes.autoHardware.getStartPose;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Enums.Alliance;
+import org.firstinspires.ftc.teamcode.Enums.StartSide;
+import org.firstinspires.ftc.teamcode.UtilClass.StartPose;
+import org.firstinspires.ftc.teamcode.opModes.autoHardware;
+import org.firstinspires.ftc.teamcode.opModes.rr.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.opModes.rr.drive.advanced.PoseStorage;
+
+@Autonomous
+//@Disabled
+public class parkAutoRR extends LinearOpMode {
+    public Pose2d startPose = autoHardware.startPose;
+    autoHardware robot = new autoHardware(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        drive.setPoseEstimate(getStartPose(Alliance.RED, StartSide.RIGHT));
+        robot.initAuto(hardwareMap);
+        if (isStopRequested()) return;
+        if (StartPose.alliance == Alliance.RED) {
+            if (StartPose.side == StartSide.RIGHT) {
+                drive.followTrajectorySequence(
+                        drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                                .lineToLinearHeading(new Pose2d(60, 60, Math.toRadians(90)))
+                                .build()
+                );
+            }
+            if (StartPose.side == StartSide.LEFT){
+                drive.followTrajectorySequence(
+                        drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                                .lineToLinearHeading(new Pose2d(36, 48, Math.toRadians(90)))
+                                .build()
+                );
+            }
+        }
+        if (StartPose.alliance == Alliance.BLUE) {
+            if (StartPose.side == StartSide.LEFT) {
+                drive.followTrajectorySequence(
+                        drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                                .lineToLinearHeading(new Pose2d(-60, 60, Math.toRadians(90)))
+                                .build()
+                );
+            }
+            if (StartPose.side == StartSide.RIGHT){
+                drive.followTrajectorySequence(
+                        drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                                .lineToLinearHeading(new Pose2d(-36, 48, Math.toRadians(90)))
+                                .build()
+                );
+            }
+        }
+        PoseStorage.currentPose = drive.getPoseEstimate();
+    }
+}
